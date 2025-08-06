8856b7cb41423db923f47ae56d64eac259771ea5
An Dang-hieu <danghieuan@gmail.com>
2025-08-06 23:37:10 +0700

    pending hash map soltion

--- README.md
+++ README.md
@@ -2,4 +2,5 @@
 
 | # | Title | Solution | Difficulty | Basic idea |
 |---| ----- | -------- | ---------- | ---------- |
-| 1 | [Two Sum](https://leetcode.com/problems/two-sum/description/) | [Python](https://github.com/danghieuan/leetcode-python/blob/main/src/0001_two-sum.py) | Easy | 1. Bruce force check every element in array O(n^2) |
+| 1 | [Two Sum](https://leetcode.com/problems/two-sum/description/) | [Python](https://github.com/danghieuan/leetcode-python/blob/main/src/0001_two-sum.py) | Easy | 1. Bruce force check every element in array O(n^2)|
+| 15 | [3Sum](https://leetcode.com/problems/3sum/description/) | [Python](https://github.com/danghieuan/leetcode-python/blob/main/src/0001_two-sum.py) | Easy | 1. Bruce force check every element in array O(n^2)|
--- src/0001_two-sum.py
+++ src/0001_two-sum.py
@@ -26,41 +26,55 @@
 
 
 
-class Solution(object):
-    def two_sum_using_two_pointer(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
+# class Solution(object):
+#     def two_sum_using_two_pointer(self, nums, target):
+#         """
+#         :type nums: List[int]
+#         :type target: int
+#         :rtype: List[int]
+#         """
         
+#         """
+#         nums = [2, 7, 11, 15], target = 9 --> result return output: [0, 1]
+#         left[i] + right[j] > target
+#         ---> reduce right[j-1]
+#         left[i] + right[j] < target
+#         ---> increment left[i+1]
+
+
+#         """
+
+
+#         # Wrong answer: 19 / 63 testcases passed
+#         left = 0 # start with index = 0
+#         right = len(sorted(nums)) - 1
+
+#         while left < right:
+#             sum = nums[left] + nums[right]
+#             if sum < target:
+#                 left +=1
+#             elif sum > target:
+#                 right -=1
+#             else:
+#                 return left, right
+#         return []
+# sol = Solution()
+
+# # Test case 1
+# print(sol.two_sum_using_two_pointer([2,7,11,15], 9))
+# # Test case 2
+# print(sol.two_sum_using_two_pointer([3,2,4], 6))
+
+
+class Solution(object):
+    def two_sum_hash_map_solution(self, nums, target):
         """
-        nums = [2, 7, 11, 15], target = 9 --> result return output: [0, 1]
-        left[i] + right[j] > target
-        ---> reduce right[j-1]
-        left[i] + right[j] < target
-        ---> increment left[i+1]
-
-
+        Hash-map solution for Pair Sum - Unsorted.
         """
+        num_map = {}
 
-
-        # Wrong answer: 19 / 63 testcases passed
-        left = 0 # start with index = 0
-        right = len(sorted(nums)) - 1
-
-        while left < right:
-            sum = nums[left] + nums[right]
-            if sum < target:
-                left +=1
-            elif sum > target:
-                right -=1
-            else:
-                return left, right
-        return []
-sol = Solution()
-
-# Test case 1
-print(sol.two_sum_using_two_pointer([2,7,11,15], 9))
-# Test case 2
-print(sol.two_sum_using_two_pointer([3,2,4], 6))
+        for i, num in enumerate(nums):
+            num_map[num] = i
+        for i, num in enumerate(nums):
+            complement = target - num
+            
